71e5a317065f147f9431c0af0b349f49fbfa09c5
void <july_ss_2017@outlook.com>
2023-07-14 20:12:01 +0800

    修复判定错误

--- core/src/main/java/com/github/julyss2019/bukkit/voidframework/common/internal/ItemSimilarPredicate.java
+++ core/src/main/java/com/github/julyss2019/bukkit/voidframework/common/internal/ItemSimilarPredicate.java
@@ -6,14 +6,14 @@ import org.bukkit.inventory.ItemStack;
 import java.util.function.Predicate;
 
 public class ItemSimilarPredicate implements Predicate<ItemStack> {
-    private final ItemStack itemStack;
+    private final ItemStack item;
 
-    public ItemSimilarPredicate(@NonNull ItemStack itemStack) {
-        this.itemStack = itemStack.clone();
+    public ItemSimilarPredicate(@NonNull ItemStack item) {
+        this.item = item.clone();
     }
 
     @Override
-    public boolean test(ItemStack item) {
-        return this.itemStack.isSimilar(itemStack);
+    public boolean test(ItemStack targetItem) {
+        return this.item.isSimilar(targetItem);
     }
 }
