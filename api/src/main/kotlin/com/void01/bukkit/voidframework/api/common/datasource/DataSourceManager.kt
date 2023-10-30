535ccc93bead71d5b5f83d3ca0c6f4242aa06cc6
void <july_ss_2017@outlook.com>
2023-10-30 19:37:29 +0800

    feat: 弃用 DataSourceManager

--- api/src/main/kotlin/com/void01/bukkit/voidframework/api/common/datasource/DataSourceManager.kt
+++ api/src/main/kotlin/com/void01/bukkit/voidframework/api/common/datasource/DataSourceManager.kt
@@ -3,6 +3,7 @@ package com.void01.bukkit.voidframework.api.common.datasource
 /**
  * 连接池管理器
  */
+@Deprecated("使用 SharedDataSourceManager")
 interface DataSourceManager {
     /**
      * 获取一个 Hikari 连接池
