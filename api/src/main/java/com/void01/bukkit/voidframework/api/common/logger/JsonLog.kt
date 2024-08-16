3f4951dc922312f9446f7ce180b4fc8d8fb1b289
void <july_ss_2017@outlook.com>
2024-08-17 00:35:48 +0800

    完善 JsonLog

--- api/src/main/java/com/void01/bukkit/voidframework/api/common/logger/JsonLog.kt
+++ api/src/main/java/com/void01/bukkit/voidframework/api/common/logger/JsonLog.kt
@@ -4,7 +4,8 @@ class JsonLog {
     private val internalKeyValueMap = mutableMapOf<String, Any>()
     val keyValueMap get() = internalKeyValueMap.toMap()
 
-    fun put(key: String, value: Any) {
+    fun put(key: String, value: Any) : JsonLog {
         internalKeyValueMap[key] = value
+        return this
     }
 }
